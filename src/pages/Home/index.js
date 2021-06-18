2965ebf0ee95d61302db9bd699e70f1acb1cae01
Lanre039 <rajilat1000@gmail.com>
2021-06-18 19:04:56 +0100

    slight fix

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,9 @@
 import React from "react";
-<<<<<<< HEAD:src/pages/Home.js
 import Typewriter from "typewriter-effect";
-import CustomButton from "../components/CustomButton";
-import logo1 from "../images/logo1.jpg";
-import Particles from "react-particles-js";
-import particlesoptions from "./particleOptions";
-=======
 import CustomButton from "../../components/CustomButton";
 import logo1 from "../../images/logo1.jpg";
->>>>>>> a97cf220df75568cfd968f25d31cdb3f818418ea:src/pages/Home/index.js
+import Particles from "react-particles-js";
+import particlesoptions from "../particleOptions";
 
 function Home() {
   return (
