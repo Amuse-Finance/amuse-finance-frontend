117dcf6f7dbba44c86fd2c7339ddd76c599815e6
Lanre039 <rajilat1000@gmail.com>
2021-06-23 16:09:53 +0100

    adjusted home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,22 +5,23 @@ import CustomButton from "../../components/CustomButton";
 import logo1 from "../../images/logo1.jpg";
 import Particles from "react-particles-js";
 import particlesoptions from "../particleOptions";
+import ethLogo from "../../assets/ethereum-logo.png";
 
 function Home() {
   return (
     <div>
       <section
-        style={{ height: "80vh" }}
-        className="relative bg-blue-300 mb-10 skeyArt"
+        style={{ height: "75vh" }}
+        className="relative bg-blue-900 flex justify-center items-center "
       >
         <div>
           <Particles
-            style={{ position: "absolute", zIndex: -1 }}
+            style={{ position: "absolute", top: 0, right: 0 }}
             params={particlesoptions}
           />
-          <section className="container mx-auto py-36 flex flex-col md:flex-row justify-between items-center">
-            <div className="w-full lg:w-1/2 text-center md:text-left">
-              <h1 className="gray-700 text-2xl md:text-5xl font-semibold">
+          <section className="container mx-auto flex flex-col md:flex-row justify-between items-center ">
+            <div className="w-full lg:w-1/2 text-center md:text-left z-50">
+              <h1 className="text-gray-50 text-2xl md:text-5xl font-semibold">
                 100% Cashbacks On
                 <span className="text-red-500">
                   <Typewriter
@@ -32,29 +33,32 @@ function Home() {
                   />
                 </span>
               </h1>
-              <p className="gray-100 md:text-2xl my-5">
+              <p className="text-gray-100 md:text-2xl my-5 ">
                 From the Dev of UniDexBot.com
               </p>
               <div>
-                <CustomButton styles="ring-8 ring-inset ring-offset-2 ring-offset-blue-600 hover:bg-blue-600 p-5 mr-3 text-blue-600 hover:text-white rounded">
+                <CustomButton styles="ring-8 ring-inset ring-offset-2 ring-offset-blue-600 hover:bg-blue-600 p-3 md:p-5 mr-3 mb-3 sm:mb-0 text-white rounded">
                   {/* BE8DA4 shadow border-blue-600 hover:bg-blue-600 border-3 border-solid */}
                   LAUNCH APP
                 </CustomButton>
-                <CustomButton styles="shadow border-blue-600 border-3 border-solid bg-blue-600 p-5 text-white rounded">
+                <CustomButton styles="shadow border-blue-600 border-3 border-solid bg-blue-600 p-3 md:p-5 text-white rounded">
                   DOWNLOAD WHITE PAPER
                 </CustomButton>
               </div>
             </div>
+            <div className="w-1/6 z-50 ethLogo">
+              <img
+                className="w-full h-full object-cover"
+                src={ethLogo}
+                alt="Ethereum logo"
+              />
+            </div>
           </section>
         </div>
       </section>
-
-      <section
-        className="bg-gray-300 text-center z-50"
-        style={{ marginTop: "-150px" }}
-      >
-        <h1 className="pt-24 text-5xl font-semibold pb-5">FEATURES</h1>
-        <p className="text-xl lg:mx-32 md:mx-10 sm:mx-2 leading-relaxed">
+      <section className="bg-indigo-300 text-center py-10">
+        <h1 className="text-4xl md:text-5xl font-semibold pb-5">FEATURES</h1>
+        <p className="px-3 text-lg md:text-xl lg:mx-32 md:mx-10 sm:mx-2 leading-relaxed">
           Duo no sed et elitr tempor dolor et ipsum. Et amet clita invidunt sit
           magna sed ut ut. Sit eos. Elitr et vero elitr diam et voluptua justo
           at accusam, vero eos sit sadipscing dolor lorem amet consetetur diam.
@@ -98,38 +102,39 @@ function Home() {
           </div>
         </div>
       </section>
-
-      <section className="md:w-full lg:w-3/4 mx-auto bg-gray-500 p-7 rounded flex flex-col sm:flex-row justify-between items-center">
-        <div>
-          <p>Convert COIN to USD</p>
-          <span className="flex ">
-            <input
-              className="w-full mr-3 outline-none p-3 bg-gray-400 rounded"
-              placeholder="Enter amount..."
-            />
-            <select className="outline-none p-3 px-4 bg-black text-white rounded">
-              <option>ETH</option>
-              <option>BTC</option>
-              <option>BNB</option>
-            </select>
-          </span>
-        </div>
-        <div className="m-3.5 sm:self-end sm:my-0">
-          <IoMdSwap className="icon text-5xl" />
-        </div>
-        <div>
-          <p>Convert COIN to USD</p>
-          <span className="flex ">
-            <input
-              className="w-full mr-3 outline-none p-3 bg-gray-400 rounded"
-              placeholder="Enter amount..."
-            />
-            <select className="outline-none p-3 px-4 bg-black text-white rounded">
-              <option>ETH</option>
-              <option>BTC</option>
-              <option>BNB</option>
-            </select>
-          </span>
+      <section className="bg-indigo-200 py-10">
+        <div className="md:w-full lg:w-3/4 mx-auto bg-gray-500 p-7 rounded flex flex-col sm:flex-row justify-between items-center">
+          <div>
+            <p>Convert COIN to USD</p>
+            <span className="flex ">
+              <input
+                className="w-full mr-3 outline-none p-3 bg-gray-400 rounded"
+                placeholder="Enter amount..."
+              />
+              <select className="outline-none p-3 px-4 bg-black text-white rounded">
+                <option>ETH</option>
+                <option>BTC</option>
+                <option>BNB</option>
+              </select>
+            </span>
+          </div>
+          <div className="m-3.5 sm:self-end sm:my-0">
+            <IoMdSwap className="icon text-5xl" />
+          </div>
+          <div>
+            <p>Convert COIN to USD</p>
+            <span className="flex ">
+              <input
+                className="w-full mr-3 outline-none p-3 bg-gray-400 rounded"
+                placeholder="Enter amount..."
+              />
+              <select className="outline-none p-3 px-4 bg-black text-white rounded">
+                <option>ETH</option>
+                <option>BTC</option>
+                <option>BNB</option>
+              </select>
+            </span>
+          </div>
         </div>
       </section>
     </div>
