11311b63bbcf3b8970c8e2f337d76ab1bdccfb22
Abdullah Zakariyya <zakariyyaopeyemi@gmail.com>
2021-07-29 07:51:10 +0100

    made some changes to Home page feature cards image size

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -158,8 +158,8 @@ export const HomeWrapper = styled.div`
 						height: 100%;
 
 						img {
-							width: 50px;
-							height: 50px;
+							width: 40px;
+							height: 40px;
 						}
 					}
 
